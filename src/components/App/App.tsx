26f5c753ac8ff7fb8cd9f126793417b770c2cf85
thiago azvdo <thiagoazvdosoares@hotmail.com>
2023-11-20 15:13:09 -0300

    adicionando sass estilização da tabela com scss

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,6 +5,7 @@ import '../Header/Header.css';
 import Button from '../../shared/Button';
 import Container from '../../shared/Button/Container/Container';
 import Input from '../../shared/Button/Input/Input';
+import Table from '../../shared/Button/Table';
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
     <div className="App">
       <Header title="Store Stock"/>
       <Container>
-       
+       <Table/>
       </Container>
     </div>
   );
--- src/shared/Button/Table/Table.tsx
+++ src/shared/Button/Table/Table.tsx
@@ -2,8 +2,36 @@ import React from 'react';
 import './Table.scss';
 
 const Table = () => {
-    return <table>
-
+    return <table className="AppTable"> {/* tabela */}
+        <thead> {/* colunas */}
+            <tr>   {/* colunas */}
+                <th>Product</th> {/* coluna 1 */}
+                <th >Price</th>   {/* coluna 2 */}
+                <th className="right">Stock</th>   {/* coluna 3 */}
+            </tr> {/* fechamento de colunas */}
+        </thead> 
+        <tbody> {/* corpo respectivo de cada coluna */}
+            <tr> {/* linha 1 */}
+                <td>Cookie</td> {/* dado da coluna 1 */}
+                <td>$1.25</td>  {/* dado da coluna 2 */}
+                <td className="right">23</td>     {/* dado da coluna 3 */}
+            </tr> {/* fechamento de dados da linha 1 */}
+            <tr>  {/* linha 2 */}
+                <td>Milk</td>   {/* dado da coluna 1 */}
+                <td>$3.50</td>  {/* dado da coluna 2 */}
+                <td className="right">10</td>     {/* dado da coluna 3 */}
+            </tr> {/* fechamento de dados da linha 2 */}
+            <tr>  {/* linha 2 */}
+                <td>Juicebox</td>   {/* dado da coluna 1 */}
+                <td>$5.50</td>  {/* dado da coluna 2 */}
+                <td className="right">15</td>     {/* dado da coluna 3 */}
+            </tr> {/* fechamento de dados da linha 2 */}
+            <tr>  {/* linha 2 */}
+                <td>PopCorn</td>   {/* dado da coluna 1 */}
+                <td>$4.00</td>  {/* dado da coluna 2 */}
+                <td className="right">12</td>     {/* dado da coluna 3 */}
+            </tr> {/* fechamento de dados da linha 2 */}
+        </tbody>
     </table>
 }
 
