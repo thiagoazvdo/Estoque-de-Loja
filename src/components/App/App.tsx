a390342c3691dee343bbecfa7771860b7f07c16b
thiago azvdo <thiagoazvdosoares@hotmail.com>
2023-11-20 11:05:22 -0300

    adicionando componente Tabela

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from '../Header';
 import './App.css';
 import '../Header/Header.css';
@@ -7,24 +7,12 @@ import Container from '../../shared/Button/Container/Container';
 import Input from '../../shared/Button/Input/Input';
 
 
-function TestComponent () {
-  return <img width="16" src="https://cdn.icon-icons.com/icons2/1129/PNG/512/searchmagnifierinterfacesymbol1_79893.png" alt="search-icon"/>
-}
-
 function App() {
-  const [street, setStreet] = useState('')
   return (
     <div className="App">
       <Header title="Store Stock"/>
       <Container>
-        <Button content="Click me"
-        onClick={() => window.alert("mensagem exemplo")}
-        appendIcon={<TestComponent />}
-        />
-        <Input label="Street"
-        placeholder="E.g: 15th Avenue"
-        value={street}
-        onChange={e => setStreet(e.target.value)}/>
+       
       </Container>
     </div>
   );
